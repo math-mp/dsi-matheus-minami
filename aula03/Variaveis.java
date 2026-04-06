7206d4925b14b3ce272909dd59becd279f9d59e6
matheus minami <matheus.minami.pacheco10@gmail.com>
2026-04-06 09:28:01 -0300

    variaveis aula 03

---
+++ aula03/Variaveis.java
@@ -0,0 +1,9 @@
+public class Variaveis{
+	public static void main(String[] args) {
+        int myNum;
+        myNum = 10;
+        String msgTeste;
+        msgTeste ="oie";
+        System.out.println(msgTeste + myNum);
+	}
+}
---
+++ aula03/variaveis2.java
@@ -0,0 +1,10 @@
+package aula03;
+
+public class variaveis2 {
+    public static void main(String[] args) {
+        int n1, n2, n3;
+        n1 = n2 = 50;
+        n3 = 5;
+        System.out.println(n1+n2+n3);
+    }
+}
