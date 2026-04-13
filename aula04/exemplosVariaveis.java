c2ed424ea4454c3114a512e6a25e8160f3652a56
matheus minami <matheus.minami.pacheco10@gmail.com>
2026-04-13 09:26:28 -0300

    tipos primitivos de variaveis e operadores de igualdade

---
+++ aula04/exemplosVariaveis.java
@@ -0,0 +1,30 @@
+package aula04;
+public class exemplosVariaveis
+{
+	public static void main(String[] args) {
+
+        //declaração de variaveis de diferentes tipos
+        int a = 993;
+        long b = 120391942;
+        byte  c= 1;
+        short d = 12312;
+        Float e = 5.23f;
+        double f =2.84d;
+        char g = 'a';
+        boolean h = false;
+        char contador = 'a';
+
+        //printando variavais na ordem apresentada
+        String digolovesanne = "eu amo a anne linda e garotas japonesas";
+        System.out.println("printando valores: " + a);
+        System.out.println("printando valores: " + b);
+        System.out.println("printando valores: " + c);
+        System.out.println("printando valores: " + d);
+        System.out.println("printando valores: " + e);
+        System.out.println("printando valores: " + f);
+        System.out.println("printando valores: " + g);
+        System.out.println("printando valores: " + h);
+        System.out.println("printando valores: " + digolovesanne);
+
+	}
+}
