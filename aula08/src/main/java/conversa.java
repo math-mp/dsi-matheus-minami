34a2fc899e37e5e55d482ed5cbb842636ea0e50b
matheus minami <matheus.minami.pacheco10@gmail.com>
2026-08-30 23:47:12 -0300

    api construction

---
+++ aula08/src/main/java/conversa.java
@@ -0,0 +1,3 @@
+public record conversa(long id, String content) {
+    
+}
---
+++ aula08/src/test/java/com/dsi/aula08/Aula08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsi.aula08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
